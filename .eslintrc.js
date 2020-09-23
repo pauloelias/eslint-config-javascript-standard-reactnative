774f2b964663804983bd41c9521d1b2594a70413
Paulo Elias <paulo@pauloelias.com>
2020-09-23 14:52:34 -0700

    Improve eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
+  plugins: ['simple-import-sort', 'react', 'react-hooks', 'react-native-a11y'],
+  extends: [
+    '@pauloelias/eslint-config-javascript-standard-core',
+    'standard-jsx',
+    'plugin:react-native-a11y/all',
+    'prettier/react'
+  ],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2020,
@@ -7,51 +14,18 @@ module.exports = {
     },
     sourceType: 'module'
   },
+  rules: {
+    'jsx-quotes': [2, 'prefer-double'],
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+    'react/jsx-uses-vars': 2,
+    'react/jsx-no-undef': 2,
+    'react/prop-types': 0,
+    'react-native-a11y/has-valid-accessibility-role': 1
+  },
   env: {
     es6: true,
     commonjs: true,
     jest: true
-  },
-  extends: [
-    'standard',
-    'prettier',
-    'standard-jsx',
-    'prettier/react',
-    'plugin:react-native-a11y/all'
-  ],
-  rules: {
-    'arrow-parens': [2, 'as-needed'],
-    camelcase: 2,
-    quotes: [
-      2,
-      'single',
-      {
-        avoidEscape: true,
-        allowTemplateLiterals: true
-      }
-    ],
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 80,
-        semi: false,
-        singleQuote: true,
-        trailingComma: 'none'
-      }
-    ],
-    'jsx-quotes': ['error', 'prefer-double'],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/jsx-uses-vars': [2],
-    'react/jsx-no-undef': 1,
-    'react-native-a11y/has-valid-accessibility-role': 1,
-    'react/prop-types': 0
-  },
-  plugins: [
-    'prettier',
-    'standard',
-    'react',
-    'react-hooks',
-    'eslint-plugin-react-native-a11y'
-  ]
+  }
 }
